d8576d9fcf47737581bc1db2426f792b9cc970d2
Dongwoo Kim <dongvvoo.kim@mail.utoronto.ca>
2024-07-30 11:13:26 +0900

    fasdfafsa

--- src/navigation.js
+++ src/navigation.js
@@ -46,16 +46,16 @@ export const footerData = {
     {
       title: 'External Resources',
       links: [
-        { text: 'Dr. John Vervaeke YouTube', href: 'https://www.youtube.com/@johnvervaeke' },
-        { text: 'UC Academic Calendar', href: 'https://artsci.calendar.utoronto.ca/section/University-College' },
-        { text: 'UC Cognitive Science', href: 'https://www.uc.utoronto.ca/cognitive-science' },
-        { text: 'United Cognitive Science Association', href: 'https://wicsc.ca/cognitive-science-associations/' },
+        { text: 'Dr. John Vervaeke YouTube', href: 'https://www.youtube.com/@johnvervaeke', target: '_blank' },
+        { text: 'UC Academic Calendar', href: 'https://artsci.calendar.utoronto.ca/section/University-College', target: '_blank' },
+        { text: 'UC Cognitive Science', href: 'https://www.uc.utoronto.ca/cognitive-science', target: '_blank' },
+        { text: 'United Cognitive Science Association', href: 'https://wicsc.ca/cognitive-science-associations/', target: '_blank' },
       ],
     },
     {
       title: 'CASA',
       links: [
-        { text: '15 King\'s College Circle\nUniversity College F301\nToronto, ON M5S 3H7', href: 'https://maps.app.goo.gl/9QRABTiaiBR5kdcg6' },
+        { text: '15 King\'s College Circle\nUniversity College F301\nToronto, ON M5S 3H7', href: 'https://maps.app.goo.gl/9QRABTiaiBR5kdcg6', target: '_blank' },
       ],
     },
   ],
@@ -64,10 +64,10 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/groups/uoftcasa/' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/casa.uoft/' },
-    { ariaLabel: 'Discord', icon: 'tabler:brand-discord', href: 'https://tinyurl.com/casa-discord' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/groups/uoftcasa/', target: '_blank' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/casa.uoft/', target: '_blank' },
+    { ariaLabel: 'Discord', icon: 'tabler:brand-discord', href: 'https://tinyurl.com/casa-discord', target: '_blank' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml'), target: '_blank' },
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
